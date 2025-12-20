39599e3f3539eb3085549b031d424ad8312c15bf
Manish Vasu <manishvasubiz@gmail.com>
2025-12-20 19:13:55 +0530

    adds keybinds to unindent lines

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,8 +9,9 @@ vim.keymap.set("v", "kj", "<Esc>", { noremap = true })
 -- bring up the Oil file explorer in a floating window
 vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>", { desc = "Open Oil in floating window"})
 
+-- lsp keymaps
 vim.keymap.set("n", "<leader>fd", vim.lsp.buf.format, { desc = "Format document" })
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open code actions" })
 vim.keymap.set("n", "<leader>oi", function()
   vim.lsp.buf.code_action({
     apply = true,
@@ -19,3 +20,9 @@ vim.keymap.set("n", "<leader>oi", function()
     },
   })
 end, { desc = "Organize imports" })
+
+
+-- Shift + Tab unindents a line
+vim.keymap.set("n", "<S-Tab>", "<<", { noremap = true, silent = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+vim.keymap.set("i", "<S-Tab>", "<C-d>", { noremap = true, silent = true })
