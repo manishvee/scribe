529ea1ee75cbf773d78662f2f2c1cdfc7ffd59a4
Manish Vasu <manishvasubiz@gmail.com>
2026-01-31 19:48:14 +0530

    updates theme and bg color

--- nvim/lua/plugins/onedark.lua
+++ nvim/lua/plugins/onedark.lua
@@ -2,13 +2,7 @@ return {
     "olimorris/onedarkpro.nvim",
     priority = 1000, -- Ensure it loads first
     config = function()
-        require("onedarkpro").setup({
-            colors = {
-                onedark_vivid = { bg = "#141414" }, -- yellow
-            }
-        })
-
-        vim.cmd("colorscheme onedark_vivid")
+        vim.cmd("colorscheme onedark")
     end
 }
 
