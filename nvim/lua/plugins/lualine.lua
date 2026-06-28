b81be091981478634e09fe1f5b3359129dd8fffe
Manish Vasu <manishvasubiz@gmail.com>
2026-06-28 20:41:13 +0530

    updates lualine color theme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         opts = {
-            theme = 'everforest',
+            theme = 'onedark',
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
